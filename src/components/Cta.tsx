9616627a760b92c7f0f3dd706969e5616f7abc83
Al'ateeq <alateeqabdullah36@gmail.com>
2025-08-20 04:21:52 +0100

    add files again

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Button } from "@/components/ui/button";
 import H2 from "./H2";
 import Description from "./Description";
 import EnrollButton from "./EnrollButton";
